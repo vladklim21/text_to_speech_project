800d90e598f0ee6c26f7d1104d33adb852005d35
vladklim21 <vladklim21@gmail.com>
2024-03-15 12:46:01 +0500

    Initial commit

--- text_to_speech_project/urls.py
+++ text_to_speech_project/urls.py
@@ -8,7 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('text_to_speech_app.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
